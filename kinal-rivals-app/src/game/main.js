2613cf4e80c1a242e6fd79992d3708244b2f2f33
jsolares-2024238 <jsolares-2024238@kinal.edu.gt>
2026-06-08 10:59:50 -0600

    feat(game): implement responsive layout and arcade physics for player mechanics

--- kinal-rivals-app/src/game/main.js
+++ kinal-rivals-app/src/game/main.js
@@ -2,30 +2,35 @@ import { Boot } from './scenes/Boot';
 import { Game } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
-import Phaser from 'phaser';
 import { Preloader } from './scenes/Preloader';
+import * as Phaser from 'phaser';
 
-// Find out more information about the Game Config at:
-// https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
+    width: window.innerWidth,
+    height: window.innerHeight,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 100 },
+            debug: false
+        }
+    },
     scene: [
+        Game,
         Boot,
         Preloader,
         MainMenu,
-        Game,
         GameOver
     ]
 };
 
-const StartGame = (parent) => {
-
+export default function StartGame(parent) {
     return new Phaser.Game({ ...config, parent });
-
 }
-
-export default StartGame;
