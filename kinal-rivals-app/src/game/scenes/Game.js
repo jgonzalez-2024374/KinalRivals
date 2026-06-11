d37d97f68a01ce15ee7ace33c3cd0ac7ed167eab
jsolares-2024238 <jsolares-2024238@kinal.edu.gt>
2026-06-11 10:06:00 -0600

    feat: prevent players from overlapping each other

--- kinal-rivals-app/src/game/scenes/Game.js
+++ kinal-rivals-app/src/game/scenes/Game.js
@@ -15,18 +15,13 @@ export class Game extends Scene {
         let altoCanvas = this.scale.height;
 
         const fondo = this.add.image(anchoCanvas / 2, altoCanvas / 2, 'bg');
-        if (fondo) {
-            fondo.setDisplaySize(anchoCanvas, altoCanvas);
-        }
-
-        const titulo = this.add.text(anchoCanvas / 2, 40, 'Kinal Rivals', {
-            fontFamily: 'Arial Black', fontSize: 32, color: '#ffffff'
-        }).setOrigin(0.5);
+        if (fondo) fondo.setDisplaySize(anchoCanvas, altoCanvas);
 
         const altoSuelo = 100;
         const posicionYSuelo = altoCanvas - (altoCanvas * 0.25); 
         this.ground = this.add.rectangle(anchoCanvas / 2, posicionYSuelo, anchoCanvas, altoSuelo, 0x000000, 0);
         this.physics.add.existing(this.ground, true);
+        this.ground.body.setSize(anchoCanvas, altoSuelo - 20);
 
         if (!this.textures.exists('txt_rojo')) {
             const canvasRojo = this.textures.createCanvas('txt_rojo', 60, 90);
@@ -44,71 +39,51 @@ export class Game extends Scene {
 
         this.player1 = this.physics.add.sprite(anchoCanvas * 0.25, posicionYSuelo - 100, 'txt_rojo');
         this.player1.setCollideWorldBounds(true);
-        this.player1.body.setGravityY(1200); // Fuerza de caída extra para el J1
+        this.player1.body.setGravityY(1200);
+        this.player1.body.setBounce(0);
 
         this.player2 = this.physics.add.sprite(anchoCanvas * 0.75, posicionYSuelo - 100, 'txt_verde');
         this.player2.setCollideWorldBounds(true);
-        this.player2.body.setGravityY(1200); // Fuerza de caída extra para el J2
+        this.player2.body.setGravityY(1200);
+        this.player2.body.setBounce(0);
 
         this.physics.add.collider(this.player1, this.ground);
         this.physics.add.collider(this.player2, this.ground);
-        this.physics.add.collider(this.player1, this.player2);
 
         this.keysWASD = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
-            down: Phaser.Input.Keyboard.KeyCodes.S,
             left: Phaser.Input.Keyboard.KeyCodes.A,
             right: Phaser.Input.Keyboard.KeyCodes.D
         });
         this.cursors = this.input.keyboard.createCursorKeys();
-
-        this.scale.on('resize', (gameSize) => {
-            const w = gameSize.width;
-            const h = gameSize.height;
-            const nuevaY = h - (h * 0.25);
-
-            this.cameras.resize(w, h);
-
-            if (fondo) {
-                fondo.setPosition(w / 2, h / 2);
-                fondo.setDisplaySize(w, h);
-            }
-            if (titulo) {
-                titulo.setPosition(w / 2, 40);
-            }
-
-            this.ground.setPosition(w / 2, nuevaY);
-            this.ground.setDisplaySize(w, altoSuelo);
-            this.ground.body.setSize(w, altoSuelo);
-        });
     }
 
     update () {
         const speed = 350;
-        const jumpForce = -650;
+        const jumpForce = -650; 
 
-        if (this.keysWASD.left.isDown) {
-            this.player1.setVelocityX(-speed);
-        } else if (this.keysWASD.right.isDown) {
-            this.player1.setVelocityX(speed);
-        } else {
-            this.player1.setVelocityX(0);
-        }
+        if (this.keysWASD.left.isDown) this.player1.setVelocityX(-speed);
+        else if (this.keysWASD.right.isDown) this.player1.setVelocityX(speed);
+        else this.player1.setVelocityX(0);
+        if (this.keysWASD.up.isDown && this.player1.body.touching.down) this.player1.setVelocityY(jumpForce);
 
-        if (this.keysWASD.up.isDown && (this.player1.body.touching.down || this.player1.body.blocked.down)) {
-            this.player1.setVelocityY(jumpForce);
-        }
+        if (this.cursors.left.isDown) this.player2.setVelocityX(-speed);
+        else if (this.cursors.right.isDown) this.player2.setVelocityX(speed);
+        else this.player2.setVelocityX(0);
+        if (this.cursors.up.isDown && this.player2.body.touching.down) this.player2.setVelocityY(jumpForce);
 
-        if (this.cursors.left.isDown) {
-            this.player2.setVelocityX(-speed);
-        } else if (this.cursors.right.isDown) {
-            this.player2.setVelocityX(speed);
-        } else {
-            this.player2.setVelocityX(0);
-        }
+        let distY = Math.abs(this.player1.y - this.player2.y);
+        let distX = Math.abs(this.player1.x - this.player2.x);
 
-        if (this.cursors.up.isDown && (this.player2.body.touching.down || this.player2.body.blocked.down)) {
-            this.player2.setVelocityY(jumpForce);
+        if (distX < 55 && distY < 80) {
+            let repulsion = 5;
+            if (this.player1.x < this.player2.x) {
+                this.player1.x -= repulsion;
+                this.player2.x += repulsion;
+            } else {
+                this.player1.x += repulsion;
+                this.player2.x -= repulsion;
+            }
         }
     }
 }
